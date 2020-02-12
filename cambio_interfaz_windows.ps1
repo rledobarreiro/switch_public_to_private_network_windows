87ce4bb1dd791952624587cfaf292af810318098
aaaarafiquem <60963172+aaaarafiquem@users.noreply.github.com>
2020-02-12 10:57:36 +0100

    Add files via upload

---
+++ cambio_interfaz_windows.ps1
@@ -0,0 +1,12 @@
+﻿#Con este comando nos devuelve un perfil de conexión de red 
+#asociado con uno de los adaptadores de red existentes
+Get-NetConnectionProfile
+
+
+# Aquí introduciremos el index de la tarjeta de red que nos interese
+# el cual sacaremos del comando anterior
+[int]$comando = Read-Host "Por favor, elija la tarjeta de red adecuada"
+
+# Una vez puesta este comando se ejecutará automáticamente, con lo que
+# cambiará la red de pública a privada
+Set-NetConnectionProfile -InterfaceIndex $comando -NetworkCategory Private
