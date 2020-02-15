fc545e92b26741c1b20e53bc11b322bc51288e6c
aaaarafiquem <60963172+aaaarafiquem@users.noreply.github.com>
2020-02-15 14:22:15 +0100

    Add files via upload

---
+++ switch_public_network_private_windows.ps1
@@ -0,0 +1,12 @@
+﻿# This command will return a network connection profile
+# associated with one of the existing network adapters
+Get-NetConnectionProfile
+
+# Here we will introduce the index of the network card that interests us
+# which we will get from the previous command
+[int]$command = Read-Host "Please, select the index of the desired network adapter"
+
+# Once this command is set, it will be executed automatically, so
+# will change the network from public to private
+Set-NetConnectionProfile -InterfaceIndex $command -NetworkCategory Private
+
